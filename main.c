11ac3b50766633303f97853ed9a35083b5f9d23d
suranjakub <xsuranj@stuba.sk>
2020-04-18 17:56:05 +0200

    Added AVL searching time in test1

--- main.c
+++ main.c
@@ -27,11 +27,26 @@ void test1() {
     end = clock();
     time_taken = (double)(end-start) / CLOCKS_PER_SEC * 1000;
     printf("AVL Insert time is: %gms\n", time_taken);
+
+    printf("\nSearching %d elements from array\n------\n", x);
+
+    //AVL tree
+    int found = 0;
+    start = clock();
+    for (int i = 0; i < x; i++)
+        if(search(avl_root, array[i]))
+            found++;
+    end = clock();
+    time_taken = (double)(end-start) / CLOCKS_PER_SEC * 1000;
+    printf("AVL Search time is: %gms\n", time_taken);
+    printf("Found %d elements", found);
+
 }
 
 int main() {
 
     test1();
 
+    printf("\n");
     return 0;
 }
