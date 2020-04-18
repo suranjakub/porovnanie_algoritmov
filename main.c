4f851e950954038020e7ac9522964cace7816f2d
suranjakub <xsuranj@stuba.sk>
2020-04-18 22:13:35 +0200

    Added search to test2

--- main.c
+++ main.c
@@ -129,6 +129,41 @@ void test2() {
     printf("RB Tree insert time is: %gms\n", time_taken);
 
     //////////////////////////////////////////////////////////
+
+    printf("\nSearching %d elements from array\n-----------------\n", x);
+
+    //AVL tree search
+    int found = 0;
+    start = clock();
+    for (int i = 0; i < x; i++)
+        if( search(avl_root, array[i]) )
+            found++;
+    end = clock();
+    time_taken = (double)(end-start) / CLOCKS_PER_SEC * 1000;
+    printf("AVL Search time is: %gms, ", time_taken);
+    printf("found %d elements\n", found);
+
+    //Hash Table Chaining search
+    found = 0;
+    start = clock();
+    for (int i = 0; i < x; i++)
+        if( hashT_search(array[i]) )
+            found++;
+    end = clock();
+    time_taken = (double)(end-start) / CLOCKS_PER_SEC * 1000;
+    printf("HT-Chaining search time is: %gms, ", time_taken);
+    printf("found %d elements", found);
+
+    //RB Tree search
+    found = 0;
+    start = clock();
+    for (int i = 0; i < x; i++)
+        if( RBsearchElement(array[i]) )
+            found++;
+    end = clock();
+    time_taken = (double)(end-start) / CLOCKS_PER_SEC * 1000;
+    printf("\nRB Tree search time is: %gms, ", time_taken);
+    printf("found %d elements", found);
 }
 
 int main() {
